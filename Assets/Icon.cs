015f22deb307125475617d9f7f980adb8261f4b1
阿保達也 <pinog.jh@gmail.com>
2019-01-22 18:00:44 +0900

    frst commit

---
+++ Assets/Icon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Icon : MonoBehaviour {
+    //=============================================================
+    public int IconNum;
+    public Sprite[] sprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    //=============================================================
+    private void Init () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Awake () {
+        Init();
+    }
+
+    private void Start () {
+        if(IconNum >= 0 && IconNum < sprite.Length) {
+            spriteRenderer.sprite = sprite[IconNum];
+        }
+    }
+
+    private void Update () {
+
+    }
+}
---
+++ Assets/Scripts/IconGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class IconGenerator : MonoBehaviour {
+    //=============================================================
+    public GameObject icon;
+
+    //=============================================================
+    private void Init () {
+    }
+
+    private void Awake () {
+        Init();
+    }
+
+    private void Start () {
+        GenerateObjects(100,5);
+    }
+
+    private void Update () {
+
+    }
+
+    //=============================================================
+    /// <summary>
+    /// オブジェクトを大量生成する
+    /// </summary>
+    private void GenerateObjects (int num,float range) {
+        for(int i = 0;i < num;i++) {
+            GenerateObject(new Vector3(Random.Range(-range,range),Random.Range(-range,range),0),(int)Random.Range(0,3));
+        }
+    }
+
+    //=============================================================
+    /// <summary>
+    /// オブジェクトを生成する
+    /// </summary>
+    private void GenerateObject (Vector3 pos,int iconNum) {
+        GameObject obj = Instantiate(icon) as GameObject;
+        obj.transform.position = pos;
+        obj.GetComponent<Icon>().IconNum = iconNum;
+    }
+}
